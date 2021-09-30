60cf9442c28008c1fccff9e2609bf47f0874df99
tqfx <tqfx@foxmail.com>
2021-09-30 23:01:56 +0800

    build(scripts): update stm32init.py

--- make.mk
+++ make.mk
@@ -1,3 +1,4 @@
+# compile gcc flags
 CFLAGS += -Wextra
 CFLAGS += -Wpedantic
 CFLAGS += -Wundef
@@ -9,12 +10,15 @@ CFLAGS += -Wfloat-equal
 CFLAGS += -Wswitch-enum
 CFLAGS += -Wswitch-default
 CFLAGS += -Wdouble-promotion
-C_INCLUDES += -I .
-C_SOURCES += $(wildcard *.c)
-LDFLAGS += -u _printf_float
-
-C_SOURCES += bsp/bsp_delay.c
-C_SOURCES += bsp/bsp_flash.c
-C_SOURCES += bsp/bsp_usart.c
-
+# C defines
+C_DEFS +=
+# C includes
 C_INCLUDES += -Ibsp
+# C sources
+C_SOURCES += $(wildcard bsp/*.c)
+# AS defines
+AS_DEFS +=
+# ASM sources
+ASM_SOURCES +=
+# link flags
+LDFLAGS += -u_printf_float
